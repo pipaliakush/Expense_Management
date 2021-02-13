aee0dc1dc351cb229053530da381a9c5321d1ba0
Mohil Zalavadiya <mohilnzalavadiya@gmail.com>
2021-02-13 19:06:58 +0530

    Added category and source models

---
+++ server/models/category.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Types.ObjectId,
+  },
+  isDefault: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Categories", CategorySchema);
