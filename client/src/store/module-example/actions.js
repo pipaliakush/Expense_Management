acd9e113b5f7ac5cffeaaf77b0fe9ce62796644e
pipaliakush <pipaliakush@gmail.com>
2021-02-14 18:49:02 +0530

    initial setup in client

--- client/src/store/module-example/actions.js
+++ client/src/store/module-example/actions.js
@@ -1,2 +1,9 @@
-export function someAction (/* context */) {
+import { api } from "boot/axios";
+import { request } from "src/store/api.js";
+
+export function login({ commit }) {
+  return request(api, "get", "/auth/google").then(response => {
+    // commit("userData", response);
+    console.log(response);
+  });
 }
