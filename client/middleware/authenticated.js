74e8e564923cab3f23adb2e19c3b0cc135e7652d
Mohil Zalavadiya <mohilnzalavadiya@gmail.com>
2021-02-20 19:36:06 +0530

    Nuxt Auth middleware changes

--- client/middleware/authenticated.js
+++ client/middleware/authenticated.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
+import jwt_decode from "jwt-decode";
 
-export default function({ redirect }) {
+export default function({ redirect, store }) {
+
+  const authDetails = Vue.$cookies.get('auth');
 
   // If the user is not authenticated
-  if (!Vue.$cookies.get('auth')) {
+  if (!authDetails) {
     return redirect("/login");
+  }
+
+  if (!store.state.user) {
+    try {
+      const tokenData = jwt_decode(authDetails, { header: true });
+      store.commit('setUser', tokenData.passport.user);
+    } catch (error) {
+      return redirect("/login");
+    }
   }
 }
