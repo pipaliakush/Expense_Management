fea5fb352328b01e68b88aafdaf48dc861706da4
Mohil Zalavadiya <mohilnzalavadiya@gmail.com>
2021-02-27 19:14:33 +0530

    Added page transition and fixed no record msg for tables

--- client/components/Sources/Table/index.vue
+++ client/components/Sources/Table/index.vue
@@ -29,9 +29,13 @@
           <td v-else>-</td>
         </tr>
       </tbody>
-      <div class="m-5 text-center" v-else>
-        No Records Found
-      </div>
+      <tbody v-else>
+        <tr>
+          <td colspan="3">
+            No Records Found
+          </td>
+        </tr>
+      </tbody>
     </template>
   </v-simple-table>
 </template>
