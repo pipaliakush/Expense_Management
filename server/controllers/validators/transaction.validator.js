eee2180632bd091994f947e306da4741a4c726eb
Gunjan Sakariya <gunjan.sakariya@appswise.com>
2021-02-28 14:09:30 +0530

    get transactions by date

--- server/controllers/validators/transaction.validator.js
+++ server/controllers/validators/transaction.validator.js
@@ -1,19 +1,31 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 const transactionSchema = Joi.object({
-  userId: Joi.objectId().required().label("User Id"),
-  type: Joi.string().valid("income", "expense").required().label("Type"),
-  sourceId: Joi.objectId().required().label("Source Id"),
-  title: Joi.string().max(50).required().label("Title"),
-  amount: Joi.number().required().label("Amount"),
-  spentOn: Joi.date().iso().required().label("Spent On"),
-  categoryId: Joi.objectId().required().label("Category Id"),
-  note: Joi.string().max(300).label("Note"),
-  image: Joi.string().label("Image"),
+  userId: Joi.objectId().required().label('User Id'),
+  type: Joi.string().valid('income', 'expense').required().label('Type'),
+  sourceId: Joi.objectId().required().label('Source Id'),
+  title: Joi.string().max(50).required().label('Title'),
+  amount: Joi.number().required().label('Amount'),
+  spentOn: Joi.date().iso().required().label('Spent On'),
+  categoryId: Joi.objectId().required().label('Category Id'),
+  note: Joi.string().allow('').max(300).label('Note'),
+  image: Joi.string().allow(null).label('Image'),
 });
 
 const validateTransaction = (source) => {
   return transactionSchema.validate(source);
 };
 
-module.exports = validateTransaction;
+const transactionQueryParamSchema = Joi.object({
+  startDate: Joi.date().iso().required().label('Start Date'),
+  endDate: Joi.date().iso().required().label('End Date'),
+});
+
+const validateTransactionQueryParams = (queryParams) => {
+  return transactionQueryParamSchema.validate(queryParams);
+};
+
+module.exports = {
+  validateTransaction,
+  validateTransactionQueryParams,
+};
